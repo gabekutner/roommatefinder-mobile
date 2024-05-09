1294fdf81a0172e0221646ee90b411a20e6a603b
gabekutner <108239517+gabekutner@users.noreply.github.com>
2024-05-09 11:12:33 -0700

    Initial commit

---
+++ App.jsx
@@ -0,0 +1,93 @@
+// import { useEffect } from 'react';
+// import { StatusBar } from 'react-native';
+
+// import './src/core/fontawesome';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+// import SplashScreen from './src/screens/SplashScreen';
+// import SignIn from './src/screens/Auth/SignIn';
+// import SignUp from './src/screens/Auth/SignUp';
+// import Home from './src/screens/Home';
+// import Message from './src/screens/Message';
+// import Search from './src/screens/Search';
+// import Onboarding from './src/screens/Onboarding';
+// import CreateProfile from './src/screens/CreateProfile';
+
+// import useGlobal from './src/core/global';
+
+
+// const Stack = createNativeStackNavigator()
+
+
+// export default function App() {
+
+//   const initialized = useGlobal(state => state.initialized)
+//   const authenticated = useGlobal(state => state.authenticated)
+//   const profileCreated = useGlobal(state => state.profileCreated)
+//   const init = useGlobal(state => state.init)
+
+//   useEffect(() => {
+//     init()
+//   }, [])
+
+//   return (
+//     <NavigationContainer>
+//       <StatusBar barStyle={'dark-content'} />
+
+//       <Stack.Navigator>
+//         {!initialized ? (
+//             <Stack.Screen 
+//               name='splash' 
+//               component={SplashScreen} 
+//               options={{ headerShown:false }} 
+//             />
+//         ) : !authenticated ? (
+//           <>
+//             <Stack.Screen 
+//               name='onboarding' 
+//               component={Onboarding} 
+//               options={{ headerShown:false }}
+//             />
+//             <Stack.Screen 
+//               name='signin' 
+//               component={SignIn} 
+//               options={{ headerShown:false }}
+//             />
+//             <Stack.Screen 
+//               name='signup' 
+//               component={SignUp} 
+//               options={{ headerShown:false }}
+//             />
+//           </>
+//         ) : !profileCreated ? (
+//           <>
+//              <Stack.Screen 
+//               name='create-profile'
+//               component={CreateProfile}
+//               options={{ headerShown:false }}
+//             />  
+//           </>
+//         ) : (
+//           <>
+//             <Stack.Screen 
+//               name='home' 
+//               component={Home} 
+//               options={{ headerShown:false }}
+//             />
+//             <Stack.Screen 
+//               name='search' 
+//               component={Search} 
+//               options={{ headerShown:false }}
+//             />
+//             <Stack.Screen 
+//               name='messages' 
+//               component={Message} 
+//               options={{ headerShown:false }}
+//             />
+//           </>
+//         ) }
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   )
+// }
---
+++ src/assets/Colors.js
@@ -0,0 +1,22 @@
+export default {
+  // primary university core colors
+  'utahRed': '#BE0000',
+  'utahBlack': '#000000',
+  'utahWhite': '#FFF',
+  // accent colors
+  'utahGranitePeak': '#708E99',
+  'utahWasatchSunrise': '#FFB81D',
+  'utahRedRocks': '#890000',
+  'utahMountainGreen': '#6CC24A',
+  'utahGreatSaltLake': '#3ABFC0',
+  'utahSaltFlatGrey': '#E2E6E6',
+  'utahZionCinderCone': '#707271',
+
+  // roommatefinder specific
+  'bg':'#E8ECF4',
+  'titleBlack': '#1E1E1E',
+  'descGrey': '#929292',
+  'labelBlack': '#222',
+  'lightGrey': '#CCC',
+  'utahRedBG': '#A60000',
+};
