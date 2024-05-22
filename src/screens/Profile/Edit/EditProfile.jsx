f2589230e38c6d6e950a8f48dfdb910e3340347c
gabekutner <108239517+gabekutner@users.noreply.github.com>
2024-05-21 22:02:35 -0600

    - [ ] Edit Profile KeyboardAvoidingView

--- src/screens/Profile/Edit/EditProfile.jsx
+++ src/screens/Profile/Edit/EditProfile.jsx
@@ -1,21 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import {
   View,
-  Text,
   StyleSheet,
   ScrollView,
-  TouchableOpacity,
-  FlatList,
+  SafeAreaView,
+  KeyboardAvoidingView,
+  Platform
 } from 'react-native';
-import { SafeAreaView } from "react-native-safe-area-context";
-// import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 
 import Input from '../../../components/Input';
 import Button from '../../../components/Button';
 import SnackBar from '../../../components/SnackBarMessage';
-import Interests from '../Create/Interests';
 
-import { interestsData } from '../../../assets/Dictionary';
 import useGlobal from '../../../core/global';
 import { colors as c } from '../../../assets/config';
 
@@ -56,95 +52,29 @@ export default function EditProfile({ navigation }) {
 
   return (
     <SafeAreaView style={{ flex:1, backgroundColor:activeColors.primary }}>
-      <ScrollView
-        showsVerticalScrollIndicator={false} 
-        style={{ padding:30 }}
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        style={{ flex: 1 }}
       >
-        <View style={{ alignItems:'center', marginBottom:50 }}>
+        <ScrollView
+          showsVerticalScrollIndicator={false} 
+          style={{ padding:30 }}
+        >
+          <View style={{ alignItems:'center', marginBottom:50 }}>
 
-          <View style={styles.section}>
-            <Input 
-              colors={activeColors}
-              label="Name"
-              editable={true}
-              secureTextEntry={false}
-              autoCapitalize={false}
-              autoCorrect={false}
-              keyboardType="default"
-              placeholder={user.name}
-              value={form.name}
-              onChangeText={name => setForm({ ...form, name })}
-              width={300}
-              height={55}
-            />
-          </View>
-
-          <View style={styles.section}>
-            <Input 
-              colors={activeColors}
-              label="Instagram"
-              editable={true}
-              secureTextEntry={false}
-              autoCapitalize={true}
-              autoCorrect={false}
-              keyboardType="default"
-              placeholder={user.instagram ? user.instagram : "@"}
-              value={form.instagram}
-              onChangeText={instagram => setForm({ ...form, instagram })}
-              width={300}
-              height={55}
-            />
-          </View>
-
-          <View style={styles.section}>
-            <Input 
-              colors={activeColors}
-              label="Snapchat"
-              editable={true}
-              secureTextEntry={false}
-              autoCapitalize={true}
-              autoCorrect={false}
-              keyboardType="default"
-              placeholder={user.snapchat ? user.snapchat : "@"}
-              value={form.snapchat}
-              onChangeText={snapchat => setForm({ ...form, snapchat })}
-              width={300}
-              height={55}
-            />
-          </View>
-
-          <View style={styles.section}>
-            <Input 
-              colors={activeColors}
-              label="Major"
-              editable={true}
-              secureTextEntry={false}
-              autoCapitalize={true}
-              autoCorrect={false}
-              keyboardType="default"
-              placeholder={user.major ? user.major : "..."}
-              value={form.major}
-              onChangeText={major => setForm({ ...form, major })}
-              width={300}
-              height={55}
-            />
-          </View>
-
-
-          <View style={{ flexDirection:'row', gap:10 }}>
             <View style={styles.section}>
               <Input 
                 colors={activeColors}
-                label="Hometown"
+                label="Name"
                 editable={true}
                 secureTextEntry={false}
-                autoCapitalize={true}
+                autoCapitalize={false}
                 autoCorrect={false}
                 keyboardType="default"
-                placeholder={user.city ? user.city : "San Francisco"}
-                value={form.city}
-                onChangeText={city => setForm({ ...form, city })}
-                width={230}
+                placeholder={user.name}
+                value={form.name}
+                onChangeText={name => setForm({ ...form, name })}
+                width={300}
                 height={55}
               />
             </View>
@@ -152,91 +82,161 @@ export default function EditProfile({ navigation }) {
             <View style={styles.section}>
               <Input 
                 colors={activeColors}
-                label="State"
+                label="Instagram"
                 editable={true}
                 secureTextEntry={false}
                 autoCapitalize={true}
                 autoCorrect={false}
                 keyboardType="default"
-                placeholder={user.state ? user.state : "CA"}
-                value={form.state}
-                onChangeText={state => setForm({ ...form, state })}
-                width={70}
+                placeholder={user.instagram ? user.instagram : "@"}
+                value={form.instagram}
+                onChangeText={instagram => setForm({ ...form, instagram })}
+                width={300}
                 height={55}
               />
             </View>
-          </View>
-          
-          <View style={styles.section}>
-            <Input 
-              colors={activeColors}
-              label="Graduation Year"
-              editable={true}
-              secureTextEntry={false}
-              autoCapitalize={true}
-              autoCorrect={false}
-              keyboardType="default"
-              placeholder={user.graduation_year ? user.graduation_year : "2028"}
-              value={form.graduation_year}
-              onChangeText={graduation_year => setForm({ ...form, graduation_year })}
-              width={300}
-              height={55}
-            />
-          </View>
 
-          <View style={styles.section}>
-            <Input 
-              colors={activeColors}
-              label="Bio"
-              editable={true}
-              secureTextEntry={false}
-              autoCapitalize={false}
-              autoCorrect={false}
-              keyboardType="default"
-              placeholder={user.description ? user.description : "A little bit about me ..."}
-              value={form.description}
-              onChangeText={description => setForm({ ...form, description })}
-              width={300}
-              height={200}
-              paddingTop={15}
-              multiline={true}
-            />
-          </View>
+            <View style={styles.section}>
+              <Input 
+                colors={activeColors}
+                label="Snapchat"
+                editable={true}
+                secureTextEntry={false}
+                autoCapitalize={true}
+                autoCorrect={false}
+                keyboardType="default"
+                placeholder={user.snapchat ? user.snapchat : "@"}
+                value={form.snapchat}
+                onChangeText={snapchat => setForm({ ...form, snapchat })}
+                width={300}
+                height={55}
+              />
+            </View>
 
-          <View style={styles.section}>
-            {/* interests here */}
+            <View style={styles.section}>
+              <Input 
+                colors={activeColors}
+                label="Major"
+                editable={true}
+                secureTextEntry={false}
+                autoCapitalize={true}
+                autoCorrect={false}
+                keyboardType="default"
+                placeholder={user.major ? user.major : "..."}
+                value={form.major}
+                onChangeText={major => setForm({ ...form, major })}
+                width={300}
+                height={55}
+              />
+            </View>
+
+
+            <View style={{ flexDirection:'row', gap:10 }}>
+              <View style={styles.section}>
+                <Input 
+                  colors={activeColors}
+                  label="Hometown"
+                  editable={true}
+                  secureTextEntry={false}
+                  autoCapitalize={true}
+                  autoCorrect={false}
+                  keyboardType="default"
+                  placeholder={user.city ? user.city : "San Francisco"}
+                  value={form.city}
+                  onChangeText={city => setForm({ ...form, city })}
+                  width={230}
+                  height={55}
+                />
+              </View>
+
+              <View style={styles.section}>
+                <Input 
+                  colors={activeColors}
+                  label="State"
+                  editable={true}
+                  secureTextEntry={false}
+                  autoCapitalize={true}
+                  autoCorrect={false}
+                  keyboardType="default"
+                  placeholder={user.state ? user.state : "CA"}
+                  value={form.state}
+                  onChangeText={state => setForm({ ...form, state })}
+                  width={70}
+                  height={55}
+                />
+              </View>
+            </View>
+            
+            <View style={styles.section}>
+              <Input 
+                colors={activeColors}
+                label="Graduation Year"
+                editable={true}
+                secureTextEntry={false}
+                autoCapitalize={true}
+                autoCorrect={false}
+                keyboardType="default"
+                placeholder={user.graduation_year ? user.graduation_year : "2028"}
+                value={form.graduation_year}
+                onChangeText={graduation_year => setForm({ ...form, graduation_year })}
+                width={300}
+                height={55}
+              />
+            </View>
+
+            <View style={styles.section}>
+              <Input 
+                colors={activeColors}
+                label="Bio"
+                editable={true}
+                secureTextEntry={false}
+                autoCapitalize={false}
+                autoCorrect={false}
+                keyboardType="default"
+                placeholder={user.description ? user.description : "A little bit about me ..."}
+                value={form.description}
+                onChangeText={description => setForm({ ...form, description })}
+                width={300}
+                height={200}
+                paddingTop={15}
+                multiline={true}
+              />
+            </View>
+
+            <View style={styles.section}>
+              {/* interests here */}
+            </View>
+            
           </View>
+        </ScrollView>
+
+        <View style={{ paddingHorizontal:45 }}>  
+          <Button
+            colors={activeColors}
+            buttonText="All Done"
+            onButtonPress={() => {
+              const resp = editProfile(form, user).then(_ => {
+                if (_.status == 200) {
+                  // success handling
+                  setShowSuccess(true)
+                } else {
+                  // error handling
+                  setShowError(true)
+                }
+              })
+            }}
+          />
         </View>
-      </ScrollView>
 
-      <View style={{ paddingHorizontal:45 }}>  
-        <Button
-          colors={activeColors}
-          buttonText="All Done"
-          onButtonPress={() => {
-            const resp = editProfile(form, user).then(_ => {
-              if (_.status == 200) {
-                // success handling
-                setShowSuccess(true)
-              } else {
-                // error handling
-                setShowError(true)
-              }
-            })
-          }}
-        />
-      </View>
+        { showSuccess ? <SnackBar colors={activeColors} message={"Profile updated successfully!"} icon={"square-check"} type={'success'} /> : <></> }
+        { showError ? <SnackBar colors={activeColors} message={"Profile update failed"} icon={"square-xmark"} type={'error'} /> : <></> }
 
-      { showSuccess ? <SnackBar colors={activeColors} message={"Profile updated successfully!"} icon={"square-check"} type={'success'} /> : <></> }
-      { showError ? <SnackBar colors={activeColors} message={"Profile update failed"} icon={"square-xmark"} type={'error'} /> : <></> }
-
+      </KeyboardAvoidingView>
     </SafeAreaView>
   )
 }
 
 
 const styles = StyleSheet.create({
-  section: {
-
-  }
+  section: {}
 })
