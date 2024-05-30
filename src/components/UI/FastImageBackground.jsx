a1350fdf7d1b7d9cbb57421e5f165053abdfe739
gabekutner <108239517+gabekutner@users.noreply.github.com>
2024-05-30 15:22:36 -0600

    remove styles folder

--- src/components/UI/FastImageBackground.jsx
+++ src/components/UI/FastImageBackground.jsx
@@ -39,7 +39,7 @@ const LoadingIndicator = () => {
 
   const backgroundColor = animatedValue.interpolate({
     inputRange: [0, 1],
-    outputRange: ['rgba(255, 255, 255, 0)', '#F0F0F0'], // Adjust colors as needed
+    outputRange: ['rgba(255, 255, 255, 0)', '#F0F0F0'],
   })
 
   return (
