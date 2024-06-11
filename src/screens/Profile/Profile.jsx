20b6fc74a8a270b4516fc63babb9079038e399be
gabekutner <108239517+gabekutner@users.noreply.github.com>
2024-06-11 15:13:34 -0600

    tiny cleanup

--- src/screens/Profile/Profile.jsx
+++ src/screens/Profile/Profile.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { 
   View,
   TouchableOpacity,
+  ScrollView,
 } from "react-native";
 
 import { launchImageLibrary } from "react-native-image-picker";
@@ -105,137 +106,138 @@ export default function ProfileScreen({ navigation }) {
   const user = useGlobal(state => state.user)
 
   return (
-    <View 
-      style={{ 
-        flex:1, 
-        alignItems:'center', 
-        paddingTop:verticalScale(80), 
-        backgroundColor:colors.primary 
-      }} 
-    >
-      <ProfileImage user={user} colors={colors} />
-      <CustomText
-        style={{
-          textAlign:'center',
-          color:colors.tint,
-          fontSize:20,
-          fontWeight:'500',
-          marginBottom:verticalScale(6),
+    <ScrollView style={{ flex:1, backgroundColor:colors.primary }}>
+      <View
+        style={{ 
+          alignItems:'center', 
+          paddingTop:verticalScale(80), 
         }}
       >
-        {user.name}
-      </CustomText>
-
-      <View style={{ justifyContent:'flex-start' }}>
-        <TouchableOpacity
-          onPress={() => {}}
+        {/* Profile Header */}
+        <ProfileImage user={user} colors={colors} />
+        <CustomText
           style={{
-            flexDirection:'row',
-            height:verticalScale(52),
-            borderRadius:26,
-            alignItems:'center',
-            paddingHorizontal:moderateScale(26),
-            marginTop:verticalScale(15)
+            textAlign:'center',
+            color:colors.tint,
+            fontSize:20,
+            fontWeight:'500',
+            marginBottom:verticalScale(6),
           }}
         >
-          <View 
-            style={{ 
-              marginRight:moderateScale(11), 
-              padding:verticalScale(6), 
-              backgroundColor:colors.secondary, 
-              borderRadius:25 
-            }}
-          >
-            <FontAwesomeIcon 
-              icon='id-badge'
-              size={verticalScale(20)}
-              color={colors.tint}
-            />
-          </View>
-          <CustomText
-            style={{
-              fontWeight:'500', 
-              color:colors.tint, 
-              fontSize:verticalScale(15)
-            }}
-          >
-            Profile
-          </CustomText>
-        </TouchableOpacity>
+          {user.name}
+        </CustomText>
 
-        <TouchableOpacity
-          onPress={() => navigation.navigate('settings')}
-          style={{
-            flexDirection:'row',
-            height:verticalScale(52),
-            borderRadius:26,
-            alignItems:'center',
-            paddingHorizontal:moderateScale(26),
-            marginTop: moderateScale(5)
-          }}
-        >
-          <View 
-            style={{ 
-              marginRight:moderateScale(11), 
-              padding:verticalScale(6), 
-              backgroundColor:colors.secondary, 
-              borderRadius:25 
-            }}
-          >
-            <FontAwesomeIcon 
-              icon='gear'
-              size={verticalScale(20)}
-              color={colors.tint}
-            />
-          </View>
-          <CustomText
+        <View style={{ justifyContent:'flex-start' }}>
+          <TouchableOpacity
+            onPress={() => {}}
             style={{
-              fontWeight:'500', 
-              color:colors.tint, 
-              fontSize:verticalScale(15)
+              flexDirection:'row',
+              height:verticalScale(52),
+              borderRadius:26,
+              alignItems:'center',
+              paddingHorizontal:moderateScale(26),
+              marginTop:verticalScale(15)
             }}
           >
-            Settings
-          </CustomText>
-        </TouchableOpacity>
+            <View 
+              style={{ 
+                marginRight:moderateScale(11), 
+                padding:verticalScale(6), 
+                backgroundColor:colors.secondary, 
+                borderRadius:25 
+              }}
+            >
+              <FontAwesomeIcon 
+                icon='id-badge'
+                size={verticalScale(20)}
+                color={colors.tint}
+              />
+            </View>
+            <CustomText
+              style={{
+                fontWeight:'500', 
+                color:colors.tint, 
+                fontSize:verticalScale(15)
+              }}
+            >
+              Profile
+            </CustomText>
+          </TouchableOpacity>
 
-        <TouchableOpacity
-          onPress={() => navigation.navigate('photo-upload')}
-          style={{
-            flexDirection:'row',
-            height:verticalScale(52),
-            borderRadius:26,
-            alignItems:'center',
-            paddingHorizontal:moderateScale(26),
-            marginTop: moderateScale(5)
-          }}
-        >
-          <View 
-            style={{ 
-              marginRight:moderateScale(11), 
-              padding:verticalScale(6), 
-              backgroundColor:colors.secondary, 
-              borderRadius:25 
-            }}
-          >
-            <FontAwesomeIcon 
-              icon='camera'
-              size={verticalScale(20)}
-              color={colors.tint}
-            />
-          </View>
-          <CustomText
+          <TouchableOpacity
+            onPress={() => navigation.navigate('settings')}
             style={{
-              fontWeight:'500', 
-              color:colors.tint, 
-              fontSize:verticalScale(15)
+              flexDirection:'row',
+              height:verticalScale(52),
+              borderRadius:26,
+              alignItems:'center',
+              paddingHorizontal:moderateScale(26),
+              marginTop: moderateScale(5)
             }}
           >
-            Add Photos
-          </CustomText>
-        </TouchableOpacity>
+            <View 
+              style={{ 
+                marginRight:moderateScale(11), 
+                padding:verticalScale(6), 
+                backgroundColor:colors.secondary, 
+                borderRadius:25 
+              }}
+            >
+              <FontAwesomeIcon 
+                icon='gear'
+                size={verticalScale(20)}
+                color={colors.tint}
+              />
+            </View>
+            <CustomText
+              style={{
+                fontWeight:'500', 
+                color:colors.tint, 
+                fontSize:verticalScale(15)
+              }}
+            >
+              Settings
+            </CustomText>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={() => navigation.navigate('photo-upload')}
+            style={{
+              flexDirection:'row',
+              height:verticalScale(52),
+              borderRadius:26,
+              alignItems:'center',
+              paddingHorizontal:moderateScale(26),
+              marginTop: moderateScale(5)
+            }}
+          >
+            <View 
+              style={{ 
+                marginRight:moderateScale(11), 
+                padding:verticalScale(6), 
+                backgroundColor:colors.secondary, 
+                borderRadius:25 
+              }}
+            >
+              <FontAwesomeIcon 
+                icon='camera'
+                size={verticalScale(20)}
+                color={colors.tint}
+              />
+            </View>
+            <CustomText
+              style={{
+                fontWeight:'500', 
+                color:colors.tint, 
+                fontSize:verticalScale(15)
+              }}
+            >
+              Add Photos
+            </CustomText>
+          </TouchableOpacity>
+        </View>
+        <ProfileLogout colors={colors} />
       </View>
-      <ProfileLogout colors={colors} />
-    </View>
+    </ScrollView>
   )
 } 
