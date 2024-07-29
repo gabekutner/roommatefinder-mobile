115d9652fa0f9a8494038b9e565bde0ff2750397
gabekutner <108239517+gabekutner@users.noreply.github.com>
2024-07-29 01:16:38 -0700

    quiz

--- src/views/Quiz/index.js
+++ src/views/Quiz/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import {SafeAreaView, View, Text, KeyboardAvoidingView, TouchableWithoutFeedback, Keyboard, Platform, ScrollView} from "react-native"
 import { useTheme, IconButton } from "react-native-paper";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { quiz } from "../../assets/Dictionary";
 
 
 function QuizView({ navigation }) {
@@ -24,13 +25,38 @@ function QuizView({ navigation }) {
         <TouchableWithoutFeedback style={{flex:1}} onPress={() => Keyboard.dismiss()}> 
           <ScrollView showsVerticalScrollIndicator={false} style={{flex:1}}>
             <View style={{gap: 10, justifyContent:'center', alignItems: 'center', paddingHorizontal:25}}>
-              <View style={{ width:200, alignItems:'center', justifyContent:'center', marginBottom:25 }}>
 
+              <View style={{ width:200, alignItems:'center', justifyContent:'center', marginBottom:25 }}>
                 <Text style={{fontSize:18, fontFamily:'NotoSans_Condensed-Regular', fontWeight:'700', color:customTheme.colors.primary, textAlign:'center'}}>
                   Roommate Matching Quiz
                 </Text>
-
               </View>
+
+              <View
+                  style={{
+                    width:'100%',
+                    backgroundColor: customTheme.colors.background,
+                    paddingHorizontal:15,
+                    paddingVertical:10,
+                    borderWidth:1,
+                    borderRadius:12,
+                    shadowColor: "#000",
+                    shadowOffset: {
+                      width: 0,
+                      height: 2,
+                    },
+                    shadowOpacity: 0.25,
+                    shadowRadius: 3.84,
+                  }}
+                >
+                  <Text style={{ fontSize:16, fontWeight:'600', color: customTheme.colors.primary,  }}>
+                    Your toothpaste has disappeared - again - and you know right where to look for it: In your roommate's bathroom tote. What do you do?
+                  </Text>
+                  <View style={{height:1.5, width:'100%', borderRadius:12, backgroundColor:customTheme.colors.primary, width:'100%', marginVertical:10}}/>
+                  
+
+
+                </View>
             </View>
           </ScrollView>
         </TouchableWithoutFeedback>
