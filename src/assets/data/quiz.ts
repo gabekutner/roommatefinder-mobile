38c6b1b1e50b21a0d39b4d6342102555e2f8469d
gabekutner <108239517+gabekutner@users.noreply.github.com>
2024-08-04 13:02:57 -0700

    update quiz options

---
+++ src/assets/data/quiz.ts
@@ -0,0 +1,77 @@
+export const quiz = [
+  {
+    id: "1",
+    question: "How's your social energy throughout the day?",
+    options: [], // no options means value is a number
+  },
+  {
+    id: "2",
+    question: "How clean do you keep your room?",
+    options: [
+      "I'm the master of cleanliness and order. 🤹",
+      "Mostly tidy, with a stray sock or two.",
+      "A bit cluttered, but I know where everything is.",
+      "Organized Chaos 😈"
+    ], // no options means value is a number
+  },
+  {
+    id: "3",
+    question: "How loud is it in your room most of the time?",
+    options: [], // no options means value is a number
+  },
+  {
+    id: "4",
+    question: "What do you think about dorm guests? 🏨",
+    options: [
+      "Guests are family—come one, come all, anytime.",
+      "Guests are cool, but they have to bring snacks to share.",
+      "Guests are okay, but keep it mellow, we're not running a hotel.",
+      "Guests? No way, this is our sanctuary."
+    ], // no options means value is a number
+  },
+  {
+    id: "5",
+    question: "How much time do you spend in the room?",
+    options: [
+      "Less than 1 hour a day",
+      "1 to 3 hours a day",
+      "4 to 6 hours a day",
+      "More than 6 hours a day"
+    ], // no options means value is a number
+  },
+  {
+    id: "6",
+    question: "How hot or cold do you keep your room?",
+    options: [], // no options means value is a number
+  },
+  {
+    id: "7",
+    question: "When is it time for bed?",
+    options: [
+      "8 pm",
+      "10 pm",
+      "12 am",
+      "2 am",
+    ], // no options means value is a number
+  },
+  {
+    id: "8",
+    question: "When do you wake up in the mornings?",
+    options: [
+      "6 am",
+      "8 am",
+      "10 am",
+      "12 pm",
+    ], // no options means value is a number
+  },
+  {
+    id: "9",
+    question: "What do you think about sharing with your roommates?",
+    options: [
+      "I'm cool with sharing everything - food, clothes, you name it … just maybe not my underwear.",
+      "I'm open to sharing some things, but not everything.",
+      "Occasional sharing is okay, but not all the time.",
+      "Sharing is too much caring - my stuff is my stuff.",
+    ], // no options means value is a number
+  },
+];
